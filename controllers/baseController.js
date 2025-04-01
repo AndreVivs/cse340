0c000c8050bb76ab16f8a4c91214fe5978dbd930
Andrea Vivas <andrevivs87@gmail.com>
2025-04-01 16:34:37 -0600

    Sessions & Messages + style

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -5,6 +5,7 @@ baseController.buildHome = async function (req, res, next) {
   try {
     const nav = await utilities.getNav();
     res.render("index", { title: "Home", nav });
+    //req.flash("notice", "This is a flash message.");
   } catch (error) {
     next(error);
   }
