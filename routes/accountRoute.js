a66fede5e2392ac1b7b4cef7b8562a3565730f47
Andrea Vivas <andrevivs87@gmail.com>
2025-04-02 08:21:30 -0600

    Deliver Registration View

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -8,6 +8,12 @@ const errorController = require("../controllers/errorController");
 // Route to handle "My Account" page request
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
+// Route to handle Registration page request
+router.get(
+  "/register",
+  utilities.handleErrors(accountController.buildRegister)
+);
+
 // Route to intentionally trigger a 500 error
 router.get("/trigger500", errorController.triggerError);
 
