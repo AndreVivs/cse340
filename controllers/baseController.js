1587a3f84b6942cc6cb841ad628b7e996749e88d
Andrea Vivas <andrevivs87@gmail.com>
2025-04-02 11:44:05 -0600

    express validation added

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -5,7 +5,6 @@ baseController.buildHome = async function (req, res, next) {
   try {
     const nav = await utilities.getNav();
     res.render("index", { title: "Home", nav });
-    //req.flash("notice", "This is a flash message.");
   } catch (error) {
     next(error);
   }
